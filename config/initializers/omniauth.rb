2abde4fd6c78bdacf4a28110997119e4aaf1b53b
Matthew William Burke <burkemw3@gmail.com>
2011-11-24 15:07:34 -0500

    add google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   provider :twitter, 'IkMQVXtoRQlS4tRxmeR9xA', 'DjEg8h7TPcYIE4XD5ekMYCH1t1GGnOZ31NtAvs65k'  
   provider :linkedin, "consumer_key", "consumer_secret"
+  provider :google_oauth2, '754768717113.apps.googleusercontent.com', 'FjuhbOH_P8chp3l3Jwggi5QI'
 end
